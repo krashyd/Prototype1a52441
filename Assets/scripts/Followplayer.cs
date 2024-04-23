4f9f73765f4cacb072f78094764b25491f904bec
krashyd <a52441@alunos.ipb.pt>
2024-04-23 15:48:16 +0100

    camera position & Make the offset into a Vector3 variable
    a52441

--- Assets/scripts/Followplayer.cs
+++ Assets/scripts/Followplayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+        transform.position = player.transform.position + offset;
     }
 }
