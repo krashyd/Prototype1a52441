8e558fedb887eec12a264f7cb9d4fe54c1afc812
krashyd <a52441@alunos.ipb.pt>
2024-04-23 15:31:21 +0100

    configure the script to move forward the car
    a52441

--- Assets/scripts/Playercontroller.cs
+++ Assets/scripts/Playercontroller.cs
@@ -13,6 +13,7 @@ public class Playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //move the vehicle forward
+        transform.Translate( Vector3.forward * Time.deltaTime * 20);
     }
 }
