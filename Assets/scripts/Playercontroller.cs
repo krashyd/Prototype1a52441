ffc5af4ee4339a095495b016dfde9f4c17ebf551
krashyd <a52441@alunos.ipb.pt>
2024-05-30 11:57:00 +0100

    Update Playercontroller.cs

--- Assets/scripts/Playercontroller.cs
+++ Assets/scripts/Playercontroller.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Playercontroller : MonoBehaviour
 {
     //private variables
-    private float speed = 5.0f;
+    private float speed = 15.0f;
     private float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
